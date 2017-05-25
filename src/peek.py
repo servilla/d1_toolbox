ddebc5a0c3ce51696dea1520dfea6174fd61dbd3
servilla <mark.servilla@gmail.com>
2017-05-25 15:20:56 -0600

    Restructure into final form for parsing sysmeta and printing in csv

--- src/peek.py
+++ src/peek.py
@@ -17,6 +17,7 @@ from __future__ import print_function
 import logging
 import sys
 
+from d1_client.mnclient_1_1 import MemberNodeClient_1_1
 from d1_client.mnclient_2_0 import MemberNodeClient_2_0
 from docopt import docopt
 
@@ -30,6 +31,36 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 logger = logging.getLogger('peek')
 
 
+def replicas(replicas=None):
+    """
+    Return list of all replicas in the format of: target node, status, and date
+    completed.
+     
+    :param replicas:
+    :type Pyxb replica class
+    :return: String of replicas
+    """
+    replica_list = []
+    for replica in replicas:
+        replicaMemberNode = replica.replicaMemberNode.value()
+        replicationStatus = replica.replicationStatus
+        replicaVerified = str(replica.replicaVerified)
+        replica_info = '"' + replicaMemberNode + ' ' + replicationStatus + ' ' + replicaVerified + '"'
+        replica_list.append(replica_info)
+
+    r = ''
+    for replica in replica_list[:-1]:
+        r += '{};'.format(replica)
+    replica = replica_list[-1:][0]
+    r += '{}'.format(replica)
+
+    return r
+
+
+def clean_pids(pids=None, dead_pids=None):
+    for pid in dead_pids:
+        if pid in pids:
+            pids.pop(pid)
 
 
 def main(argv):
@@ -37,8 +68,8 @@ def main(argv):
     Peeks at an object's system metadata
     
     Usage: 
-        peek.py file <file> [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
-        peek.py pid <pid> [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
+        peek.py file <file> [-n | --node <node>] [--v1] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
+        peek.py pid <pid> [-n | --node <node>] [--v1] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
         peek.py -h | --help
 
     Arguments:
@@ -51,17 +82,20 @@ def main(argv):
         -c --cert   Client certificate
         -k --key    Client certificate key
         -a --attr   Comma separated list of sysmeta attributes to display (none for all)
+        --v1        Version 1 client (defaults to version 2)
         --header    Add header line
 
     """
     args = docopt(str(main.__doc__))
 
-    pids = []
+    pids = {}
     if args['pid']:
-        pids.append(args['<pid>'])
+        pids[args['<pid>']] = {'pid': args['<pid>']}
     elif args['file']:
         with open(args['<file>'], 'r') as fn:
-            pids = [_.strip() for _ in fn.readlines()]
+            pid_list = [_.strip() for _ in fn.readlines()]
+            for pid in pid_list:
+                pids[pid] = {'pid': pid}
 
     if args['--node']:
         base_url = args['<node>']
@@ -86,32 +120,51 @@ def main(argv):
     if args['--header']:
         add_header_line = True
 
-    attributes = None
+    attributes = ()
     if args['--attr']:
         attributes = [ _.strip() for _ in args['<attr>'].split(',')]
 
-    mn_client = MemberNodeClient_2_0(base_url=base_url,
-                                     cert_pem_path=cert_pem_path,
-                                     cert_key_path=cert_key_path,
-                                     verify_tls=properties.VERIFY_TLS,
-                                     )
+    if args['--v1']:
+        client = MemberNodeClient_1_1(base_url=base_url,
+                                      cert_pem_path=cert_pem_path,
+                                      cert_key_path=cert_key_path,
+                                      verify_tls=properties.VERIFY_TLS,
+                                      )
+    else:
+        client = MemberNodeClient_2_0(base_url=base_url,
+                                         cert_pem_path=cert_pem_path,
+                                         cert_key_path=cert_key_path,
+                                         verify_tls=properties.VERIFY_TLS,
+                                         )
+
+    dead_pids = []
+    for pid in pids:
+        try:
+            sm = client.getSystemMetadata(pid=pid)
+            if 'checksum' in attributes:
+                pids[pid]['checksum'] = sm.checksum.algorithm + ',' + sm.checksum.value()
+            if 'size' in attributes:
+                pids[pid]['size'] = sm.size
+            if 'replica' in attributes:
+                pids[pid]['replica'] = replicas(sm.replica)
+        except Exception as e:
+            logger.error('{pid}: {e}'.format(pid=pid, e=e))
+            dead_pids.append(pid)
+
+    clean_pids(pids, dead_pids)
 
     for pid in pids:
-        sm = mn_client.getSystemMetadata(pid=pid)
-        replicas = sm.replica
-        replica_list = []
-        for replica in replicas:
-            replicaMemberNode = replica.replicaMemberNode.value()
-            replicationStatus = replica.replicationStatus
-            replicaVerified = str(replica.replicaVerified)
-            replica_info = '"' + replicaMemberNode + ' ' + replicationStatus + ' ' + replicaVerified + '"'
-            replica_list.append(replica_info)
-
-        print('{}'.format(sm.identifier.value()), end=', ')
-        for replica in replica_list[:-1]:
-            print('{}'.format(replica), end=', ')
-        replica = replica_list[-1:][0]
-        print('{}'.format(replica))
+        print('{pid}'.format(pid=pid), end=',')
+        if 'checksum' in attributes:
+            checksum = pids[pid]['checksum']
+            print('{checksum}'.format(checksum=checksum), end=',')
+        if 'size' in attributes:
+            size = pids[pid]['size']
+            print('{size}'.format(size=size), end=',')
+        if 'replica' in attributes:
+            replica = pids[pid]['replica']
+            print('{replica}'.format(replica=replica), end=',')
+        print()
 
     return 0
 
