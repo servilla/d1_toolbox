b952ffec37f77dd10dd64eaac22aa61a3ce3ea63
servilla <mark.servilla@gmail.com>
2017-05-31 12:31:29 -0600

    Add docopts CLI tools

--- src/delete_objects.py
+++ src/delete_objects.py
@@ -19,33 +19,87 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
 
 logger = logging.getLogger('delete_objects')
 
+from docopt import docopt
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+from d1_client.mnclient_1_1 import MemberNodeClient_1_1
 from d1_client.mnclient_2_0 import MemberNodeClient_2_0
 from d1_client.iter.objectlist import ObjectListIterator
 import properties
 
 
 def main():
+    """
+    Delete object(s) from a DataONE Member Node
 
-    mn_client = MemberNodeClient_2_0(base_url=properties.BASE_URL,
-                                     cert_pem_path=properties.CERT_PEM,
-                                     cert_key_path=properties.CERT_KEY,
-                                     verify_tls=properties.VERIFY_TLS,
-                                     )
+    Usage:
+        delete_objects.py pid <pid> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>]
+        delete_objects.py file <file> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>]
+        delete_objects.py -h | --help
 
-    objects = ObjectListIterator(client=mn_client)
-    cnt = len(objects) - 3
-    n = 0
+    Arguments:
+        pid    Pid of object to delete
+        file   File containing pid(s) (one per line) of objects to delete
 
-    for obj in objects:
-        if obj.identifier.value() not in ('knb.1303.1', 'knb.1305.1', 'knb.1308.1'):
-            n += 1
-            print('Deleting {n} of {cnt}: {pid}'.format(n=n, cnt=cnt, pid=obj.identifier.value()))
-            #mn_client.delete(pid=obj.identifier.value())
+    Options:
+        -h --help   This page
+        -n --node   Target node (either member or coordinating)
+        -c --cert   Client certificate
+        -k --key    Client certificate key
+        --v1        Version 1 client (defaults to version 2)
+
+    """
+
+    args = docopt(str(main.__doc__))
+
+    pids = []
+    if args['pid']:
+        pids.append(args['<pid>'])
+    elif args['file']:
+        with open(args['<file>'], 'r') as fn:
+            pids = [_.strip() for _ in fn.readlines()]
+
+    if args['--node']:
+        base_url = args['<node>']
+    else:
+        base_url = properties.BASE_URL
+
+    if args['--cert']:
+        cert_pem_path = args['<cert>']
+        if cert_pem_path == 'None':
+            cert_pem_path = None
+    else:
+        cert_pem_path = properties.CERT_PEM
+
+    if args['--key']:
+        cert_key_path = args['<key>']
+        if cert_key_path == 'None':
+            cert_key_path = None
+    else:
+        cert_key_path = properties.CERT_KEY
+
+    if args['--v1']:
+        client = MemberNodeClient_1_1(base_url=base_url,
+                                      cert_pem_path=cert_pem_path,
+                                      cert_key_path=cert_key_path,
+                                      verify_tls=properties.VERIFY_TLS,
+                                      )
+    else:
+        client = MemberNodeClient_2_0(base_url=base_url,
+                                         cert_pem_path=cert_pem_path,
+                                         cert_key_path=cert_key_path,
+                                         verify_tls=properties.VERIFY_TLS,
+                                         )
+
+    for pid in pids:
+        logger.warn('Deleting: {pid}'.format(pid=pid))
+        try:
+            client.delete(pid=pid)
+        except Exception as e:
+            logger.error(e)
         
     return 0
 
