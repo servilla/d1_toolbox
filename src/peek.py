2a1bceabe32f8aac987e1bdda493324bd7f95284
servilla <mark.servilla@gmail.com>
2017-09-21 15:30:14 -0600

    Handle optional content; add pretty print xml method

--- src/peek.py
+++ src/peek.py
@@ -20,6 +20,7 @@ import sys
 from d1_client.mnclient_1_1 import MemberNodeClient_1_1
 from d1_client.mnclient_2_0 import MemberNodeClient_2_0
 from docopt import docopt
+from lxml import etree
 
 import properties
 
@@ -29,6 +30,17 @@ logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger('peek')
+
+
+def clean_pids(pids=None, dead_pids=None):
+    for pid in dead_pids:
+        if pid in pids:
+            pids.pop(pid)
+
+
+def pretty_xml(xml=None):
+    root = etree.fromstring(xml)
+    return etree.tostring(root, pretty_print=True)
 
 
 def replicas(replicas=None):
@@ -57,19 +69,13 @@ def replicas(replicas=None):
     return r
 
 
-def clean_pids(pids=None, dead_pids=None):
-    for pid in dead_pids:
-        if pid in pids:
-            pids.pop(pid)
-
-
 def main(argv):
     """
     Peeks at a DataONE Member Node object's system metadata
     
     Usage: 
-        peek.py pid <pid> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
-        peek.py file <file> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header]
+        peek.py pid <pid> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header] [--to_xml]
+        peek.py file <file> [--v1] [-n | --node <node>] [-c | --cert <cert>] [-k | --key <key>] [-a | --attr <attr>] [--header] [--to_xml]
         peek.py defaults
         peek.py -h | --help
 
@@ -86,6 +92,7 @@ def main(argv):
         -a --attr   Comma separated list of sysmeta attributes to display (none for all)
         --v1        Version 1 client (defaults to version 2)
         --header    Add header line
+        --to_xml    Export as XML
 
     """
     args = docopt(str(main.__doc__))
@@ -130,7 +137,11 @@ def main(argv):
     if args['--attr']:
         attributes = [ _.strip() for _ in args['<attr>'].split(',')]
     else:
-        attributes = ['checksum', 'size', 'replica', 'archived']
+        attributes = ['checksum', 'size', 'replica', 'archived', 'rights_holder']
+
+    to_xml = False
+    if args['--to_xml']:
+        to_xml = True
 
     if args['--v1']:
         client = MemberNodeClient_1_1(base_url=base_url,
@@ -149,35 +160,52 @@ def main(argv):
     for pid in pids:
         try:
             sm = client.getSystemMetadata(pid=pid)
-            if 'checksum' in attributes:
-                pids[pid]['checksum'] = sm.checksum.algorithm + ',' + sm.checksum.value()
-            if 'size' in attributes:
-                pids[pid]['size'] = sm.size
-            if 'replica' in attributes:
-                pids[pid]['replica'] = replicas(sm.replica)
-            if 'archived' in attributes:
-                pids[pid]['archived'] = sm.archived
+            pids[pid]['xml'] = sm.toxml()
         except Exception as e:
             logger.error('{pid}: {e}'.format(pid=pid, e=e))
             dead_pids.append(pid)
 
+        if 'checksum' in attributes:
+            pids[pid]['checksum'] = sm.checksum.algorithm + ',' + sm.checksum.value()
+        if 'size' in attributes:
+            pids[pid]['size'] = sm.size
+        if 'replica' in attributes:
+            try:
+                pids[pid]['replica'] = replicas(sm.replica)
+            except Exception as e:
+                pids[pid]['replica'] = ''
+        if 'archived' in attributes:
+            try:
+                pids[pid]['archived'] = sm.archived
+            except Exception as e:
+                pids[pid]['archived'] = ''
+        if 'rights_holder' in attributes:
+            pids[pid]['rights_holder'] = '\'' + sm.rightsHolder.value() + '\''
+
     clean_pids(pids, dead_pids)
 
     for pid in pids:
-        print('{pid}'.format(pid=pid), end=',')
-        if 'checksum' in attributes:
-            checksum = pids[pid]['checksum']
-            print('{checksum}'.format(checksum=checksum), end=',')
-        if 'size' in attributes:
-            size = pids[pid]['size']
-            print('{size}'.format(size=size), end=',')
-        if 'replica' in attributes:
-            replica = pids[pid]['replica']
-            print('{replica}'.format(replica=replica), end=',')
-        if 'archived' in attributes:
-            archived = pids[pid]['archived']
-            print('{archived}'.format(archived=archived), end=',')
-        print()
+        if to_xml:
+            xml = pretty_xml(pids[pid]['xml'])
+            print('{xml}'.format(xml=xml))
+        else:
+            print('{pid}'.format(pid=pid), end=',')
+            if 'checksum' in attributes:
+                checksum = pids[pid]['checksum']
+                print('{checksum}'.format(checksum=checksum), end=',')
+            if 'size' in attributes:
+                size = pids[pid]['size']
+                print('{size}'.format(size=size), end=',')
+            if 'replica' in attributes:
+                replica = pids[pid]['replica']
+                print('{replica}'.format(replica=replica), end=',')
+            if 'archived' in attributes:
+                archived = pids[pid]['archived']
+                print('{archived}'.format(archived=archived), end=',')
+            if 'rights_holder' in attributes:
+                rights_holder = pids[pid]['rights_holder']
+                print('{rights_holder}'.format(rights_holder=rights_holder), end=', ')
+            print()
 
     return 0
 
