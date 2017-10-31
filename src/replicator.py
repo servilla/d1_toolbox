e80a1a5fb3eb3b4c23bcc1e2b717cd9de32fd033
servilla <mark.servilla@gmail.com>
2017-10-30 21:05:33 -0600

    Add replication with modified system metadata

--- src/replicator.py
+++ src/replicator.py
@@ -15,26 +15,67 @@
 import sys
 import logging
 
-from docopt import docopt
+from d1_client.mnclient_2_0 import MemberNodeClient_2_0
+import d1_common.types.exceptions
+import d1_common.types.dataoneTypes_v2_0 as dataoneTypes_v2_0
 import daiquiri
+from docopt import docopt
 
 import properties
 
 daiquiri.setup(level=logging.INFO)
-
 logger = daiquiri.getLogger('replicator.py: ' + __name__)
 
 
-def get_object(pid=None, src=None):
+def get_local_object(pid_file=None):
     pass
+
+
+def get_remote_object():
+    pass
+
+
+def get_object(pid=None, src=None):
+    src_mn = src['src_mn']
+    if src_mn[0] == '/':
+        # Local directory
+        pid_file = src_mn + '/' + pid
+        return open(pid_file, 'rb')
+    else:
+        return get_remote_object()
 
 
 def get_sys_meta(pid=None, src=None):
-    pass
+    src_mn = src['src_mn']
+    if src_mn[0] == '/':
+        # Local directory
+        sys_meta_file = src_mn + '/' + pid + '.xml'
+        with open(sys_meta_file, 'rt') as f:
+            xml_text = f.read()
+        return dataoneTypes_v2_0.CreateFromDocument(xml_text=xml_text)
+    else:
+        return get_remote_object()
 
 
-def replicate_object(pid=None, obj=None, sys_meta=None, dst=None):
-    pass
+def replicate_object(pid=None, obj=None, sys_meta=None):
+    mn_client = MemberNodeClient_2_0(base_url=properties.BASE_URL,
+                                     cert_pem_path=properties.CERT_PEM,
+                                     cert_key_path=properties.CERT_KEY,
+                                     verify_tls=properties.VERIFY_TLS,
+                                     )
+    mn_client.create(pid=pid, obj=obj, sysmeta_pyxb=sys_meta)
+    return 0
+
+
+def modify_sys_meta(sys_meta=None):
+    sys_meta.replicationPolicy.numberReplicas = 0
+    sys_meta.replicationPolicy.replicationAllowed=False
+    sys_meta.replica = None
+    sys_meta.authoritativeMemberNode = 'urn:node:EDI'
+    sys_meta.submitter = 'CN=urn:node:EDI,DC=dataone,DC=org'
+    sys_meta.dateSysMetadataModified = None
+    sys_meta.obsoletes = None
+    sys_meta.obsoletedBy = None
 
 
 def main(argv):
@@ -80,12 +121,12 @@ def main(argv):
         pids.append(args['<pid>'])
     else:
         try:
-            f = open(args['<file>'], 'r')
+            with open(args['<file>'], 'rt') as f:
+                for pid in f:
+                    pids.append(pid.strip())
         except IOError as e:
             logger.error(e)
             return 1
-        for pid in f:
-            pids.append(pid.strip())
 
     src_mn = args['<src_mn>']
     if args['--cert'] and args['--key']:
@@ -96,17 +137,20 @@ def main(argv):
         src_key =None
 
     src = {'src_mn': src_mn, 'src_cert': src_cert, 'src_key': src_key}
-        
-    dst_mn = properties.BASE_URL
-    dst_cert = properties.CERT_PEM
-    dst_key = properties.CERT_KEY
 
-    dst = {'dst_mn': dst_mn, 'dst_cert': dst_cert, 'dst_key': dst_key}
-
+    logger.info(len(pids))
+    cnt = 0
     for pid in pids:
+        cnt += 1
+        information = '{cnt}: {pid}'.format(cnt=cnt, pid=pid)
+        logger.info(information)
         obj = get_object(pid=pid, src=src)
         sys_meta = get_sys_meta(pid=pid, src=src)
-        replicate_object(pid=pid, obj=obj, sys_meta=sys_meta, dst=dst)
+        modify_sys_meta(sys_meta=sys_meta)
+        try:
+            replicate_object(pid=pid, obj=obj, sys_meta=sys_meta)
+        except d1_common.types.exceptions.DataONEException as e:
+            logger.error(e)
 
 
 
